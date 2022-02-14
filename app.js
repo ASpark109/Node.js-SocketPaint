97369ed5b77e1920d3ebdf967816964e3ce2daaf
KLiubomyr <75342698+ASpark109@users.noreply.github.com>
2022-02-14 17:31:52 +0200

    Additional functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express');
 let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let fs = require('fs');
 
 let bitField = [];
 
@@ -12,26 +13,53 @@ app.get('/', function(req, res) {
 app.use(express.static(__dirname + '/staticfile'))
 
 io.on('connection', function(socket) {
-    console.log('A user connected');
-    
-    socket.emit('updata', bitField)
-    //Whenever someone disconnects this piece of code executed
-    socket.on('disconnect', function () {
-       console.log('A user disconnected');
-    });
+    socket.emit('nameSet');
 
-    socket.on('clickTrue', function (x) {
-        console.log('A user clicked true - ' + x);
-        bitField[x] = true;
-        io.sockets.emit('broadcast', x, true);
-    });
-    socket.on('clickFalse', function (x) {
-        console.log('A user clicked false - ' + x);
-        bitField[x] = false;
-        io.sockets.emit('broadcast', x, false);
-    });
+    socket.on('newNameSet', (name) => {
+        let time = new Date();
+        logDataAppend("New user \"" + name + "\" connected " + 
+            time.getHours() + ":" + 
+            time.getMinutes() + ":" + 
+            time.getSeconds() + " /" + 
+            time.getDate() + "/" + 
+            time.getMonth() + "/" + 
+            time.getFullYear() + "\n");
+
+        socket.broadcast.emit('newUser', name)
+
+        socket.emit('updata', bitField)
+   
+        socket.on('disconnect', function () {
+           console.log('A user disconnected');
+        });
+    
+        socket.on('clickTrue', function (x, UserActName) {
+            logDataAppend("U " + UserActName + " (t) " + x + "\n")
+            bitField[x] = true;
+            io.sockets.emit('broadcast', x, true);
+            socket.broadcast.emit('userAction', UserActName, x)
+        });
+
+        socket.on('clickFalse', function (x, UserActName) {
+            logDataAppend("U " + UserActName + " (f) " + x + "\n")
+            bitField[x] = false;
+            io.sockets.emit('broadcast', x, false);
+            socket.broadcast.emit('userAction', UserActName, x)
+        });
+    })
  });
 
 http.listen(3000, function() {
     console.log('listening on localhost:3000');
  });
+
+
+function logDataAppend(data)
+{
+    fs.appendFile('logfile.txt', data, 'utf-8', (err)=>{
+        if(err)
+        {
+            console.log(err);
+        }
+    })
+}
